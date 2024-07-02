f5ee5006919b63b1118e011064672cd6b405e2c7
Jeel Patel <itsjeel01@Jeels-MacBook-Pro.local>
2024-07-02 17:39:16 +0530

    Add back press constraints from dashboard

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:skillshunt/screens/insights_screen.dart';
 import 'package:skillshunt/screens/my_learning_screen.dart';
 
@@ -24,22 +25,30 @@ class _DashboardScreenState extends State<DashboardScreen> {
     Widget activeScreen =
         _selectedPageIndex == 0 ? const MyLearningScreen() : const InsightsScreen();
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(activePageTitle),
-        centerTitle: false,
-        automaticallyImplyLeading: false,
-      ),
-      body: activeScreen,
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (index) {
-          _setPageIndex(index);
-        },
-        currentIndex: _selectedPageIndex,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.code_rounded), label: 'My Learning'),
-          BottomNavigationBarItem(icon: Icon(Icons.auto_graph_rounded), label: 'Insights')
-        ],
+    return PopScope(
+      canPop: true,
+      onPopInvoked: (didPop) {
+        if (!didPop) {
+          SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(activePageTitle),
+          centerTitle: false,
+          automaticallyImplyLeading: false,
+        ),
+        body: activeScreen,
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            _setPageIndex(index);
+          },
+          currentIndex: _selectedPageIndex,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.code_rounded), label: 'My Learning'),
+            BottomNavigationBarItem(icon: Icon(Icons.auto_graph_rounded), label: 'Insights')
+          ],
+        ),
       ),
     );
   }
