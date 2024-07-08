0a08e101661cefa14715d7594e8c88de58f9cb85
Jeel Patel <itsjeel01@Jeels-MacBook-Pro.local>
2024-07-08 17:39:20 +0530

    Change skill tile title on demand

--- lib/screens/my_learning_screen.dart
+++ lib/screens/my_learning_screen.dart
@@ -2,9 +2,8 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:skillshunt/data/gradients.dart';
+
 import 'package:skillshunt/providers/skills_provider.dart';
-import 'package:skillshunt/widgets/skill_tile.dart';
 
 class MyLearningScreen extends ConsumerStatefulWidget {
   const MyLearningScreen({super.key});
@@ -26,48 +25,21 @@ class _MyLearningScreenState extends ConsumerState<MyLearningScreen> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'Ongoing Skills',
-              style: Theme.of(context).textTheme.titleMedium!.copyWith(fontWeight: FontWeight.bold),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Ongoing Skills',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium!
+                      .copyWith(fontWeight: FontWeight.bold),
+                ),
+                const Text('View all'),
+              ],
             ),
             const SizedBox(
               height: 8,
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                    decoration: BoxDecoration(
-                        gradient: const LinearGradient(
-                          colors: [Color(0xff16222a), Color(0xff3a6073)],
-                          stops: [0, 1],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                        ),
-                        borderRadius: BorderRadius.circular(8)),
-                    child: Column(
-                      children: [
-                        const Icon(
-                          Icons.add_circle_outline_rounded,
-                          color: Colors.white60,
-                        ),
-                        const SizedBox(height: 8),
-                        Text(
-                          'New Skill',
-                          style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                                fontWeight: FontWeight.w600,
-                                color: Colors.white60,
-                              ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  for (final skill in selectedSkills)
-                    SkillTile(randomIndex: random.nextInt(gradients.length))
-                ],
-              ),
             ),
           ],
         ),
--- lib/widgets/skill_tile.dart
+++ lib/widgets/skill_tile.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:skillshunt/data/gradients.dart';
 
 class SkillTile extends StatelessWidget {
-  const SkillTile({required this.randomIndex, super.key});
+  const SkillTile({
+    required this.title,
+    required this.randomIndex,
+    super.key,
+  });
 
   final int randomIndex;
+  final String title;
 
   @override
   Widget build(BuildContext context) {
@@ -12,21 +17,12 @@ class SkillTile extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
       decoration:
           BoxDecoration(gradient: gradients[randomIndex], borderRadius: BorderRadius.circular(8)),
-      child: Column(
-        children: [
-          const Icon(
-            Icons.add_circle_outline_rounded,
-            color: Colors.white60,
-          ),
-          const SizedBox(height: 8),
-          Text(
-            'New Skill',
-            style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                  fontWeight: FontWeight.w600,
-                  color: Colors.white60,
-                ),
-          ),
-        ],
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.labelLarge!.copyWith(
+              fontWeight: FontWeight.w600,
+              color: Colors.white60,
+            ),
       ),
     );
   }
